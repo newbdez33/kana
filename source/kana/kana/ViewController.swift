db19cf6f12546d06d7ad056098d3d8723b284c98
Jacky <newbdez33@gmail.com>
2017-01-07 22:01:58 +0900

    add source project

---
+++ source/kana/kana/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kana
+//
+//  Created by JackyZ on 2017/01/07.
+//  Copyright © 2017年 Salmonapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
